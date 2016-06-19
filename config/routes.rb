57704c1782290e480665181260cb6e9726af9615
cedricdlb <cdlb.git@gmail.com>
2016-06-18 17:31:48 -0700

    Add vim swap files and mac's DS_Store to the .gitignore file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :games do
+    resources :moves
+  end
+  resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
