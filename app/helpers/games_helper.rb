ab0d99b82b941c412ae24092b81dd007ae23559a
cedricdlb <cdlb.git@gmail.com>
2016-09-04 01:02:04 -0700

    First pass at adding graphics to the game.  Game#show displays one board an aircraft carrier, white pegs in all the holes with their positions adjusted to account for the skew of the board image and red pegs on the four corners and along all of the ship.  The helpers/games_helper.rb has a method to calculate the left and top absolute positioning of a peg based on the target coordinate.
    Next steps are to pass in the player id (or later use devise for loging
    in, user authentication, and thus know current_user's id).  for a player
    have the controller pass in the coordinates of the player's ships and
    the set of shots fired at the player so far and to display the ships and
    white or red pegs at the target locations.
    Then to display the targetting board and the shots this player fired,
    and I think the identity of known sunk ships.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,19 @@
 module GamesHelper
+  def peg_position(coordinate, board_image_size)
+    # TODO: Adapt to variable board size
+    first_hole_top=59
+    first_hole_left=55
+    letter = coordinate[0].downcase
+    y_coord = "abcdefghij".index(letter)
+    x_coord = coordinate[1..-1].to_i - 1 # -1 for 0-based sequence
+
+    left = first_hole_left + 50.5*x_coord
+    left += (4.5-x_coord)*((8.5-y_coord)/3)
+
+    top  = first_hole_top  + 50.5*y_coord
+    top  -=   y_coord  *2 if y_coord <  5
+    top  -= (9-y_coord)*2 if y_coord >= 5
+
+    return [left, top]
+  end
 end
