19d83bba7df46ea6375a9f8bbe48a4dad1949c14
cedricdlb <cdlb.git@gmail.com>
2016-09-05 14:52:50 -0700

    board image was corrected to remove the photo's parallax skew and make a truly square board, simplifying board position calculations. Changed images of ships to have transparent backgrounds games_helper:   square_position finds a coordinate's left-top position on a sized board   peg_position calls square_position   ship_position calls square_position   peg_tag outputs the html needed to position and display a white or red     peg, including its shadow games/show:   Partial abstraction of board size   use peg_tag helper method rather than clutter view   Added horizontal and vertical copies of all the ships on the board TODO: Add ship_tag helper method to unclutter view

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,19 +1,68 @@
 module GamesHelper
-  def peg_position(coordinate, board_image_size)
-    # TODO: Adapt to variable board size
-    first_hole_top=59
-    first_hole_left=55
+  def square_position(coordinate, board_image_size)
+    each_square_size = board_image_size / 11.0
+
+    # First column and row are legends (A-J, 1-9),
+    # so x,y coords are 1-based rather than 0-based
+    # i.e. elements in col 1 (e.g. coordinate "F1")
+    # will start at square_size * 1, skipping the
+    # first column's legend
     letter = coordinate[0].downcase
-    y_coord = "abcdefghij".index(letter)
-    x_coord = coordinate[1..-1].to_i - 1 # -1 for 0-based sequence
-
-    left = first_hole_left + 50.5*x_coord
-    left += (4.5-x_coord)*((8.5-y_coord)/3)
-
-    top  = first_hole_top  + 50.5*y_coord
-    top  -=   y_coord  *2 if y_coord <  5
-    top  -= (9-y_coord)*2 if y_coord >= 5
+    y_coord = "Xabcdefghij".index(letter)
+    x_coord = coordinate[1..-1].to_i
+    left = each_square_size * x_coord 
+    top  = each_square_size * y_coord
 
     return [left, top]
+  end
+
+  def peg_position(coordinate, board_image_size, peg_size)
+    each_square_size = board_image_size / 11.0
+
+    left, top = square_position(coordinate, board_image_size)
+
+    left += (each_square_size - peg_size)/2
+    top  += (each_square_size - peg_size)/2
+
+    return [left, top]
+  end
+
+  # ship offsets (to line up the holes in the ships' images with the holes in the board)
+  # were calculated based on a board size of 550, which is why they are divided by 550
+  # and later multiplied by the actual board size.
+  SHIP_OFFSETS = {
+  # SHIP_MASK                   => { horizontal: [left, top],         vertical: [left, top] },
+    Game::SUBMARINE_MASK        => { horizontal: [0, 0],              vertical: [0, 0] },
+    Game::CRUISER_MASK          => { horizontal: [0, 0],              vertical: [0, 2.0/550] },
+    Game::BATTLESHIP_MASK       => { horizontal: [0, 0],              vertical: [0, 3.0/550] },
+    Game::DESTROYER_MASK        => { horizontal: [3.0/550, 0],        vertical: [1.0/550, -1.0/550] },
+    Game::AIRCRAFT_CARRIER_MASK => { horizontal: [2.0/550, -1.0/550], vertical: [0, 2.0/550] },
+  }
+
+  def ship_position(coordinate, board_image_size, ship_mask, orientation)
+    left, top = square_position(coordinate, board_image_size)
+    delta_left, delta_top = SHIP_OFFSETS[ship_mask][orientation] 
+
+    left += delta_left * board_image_size
+    top  += delta_top  * board_image_size
+
+    return [left, top]
+  end
+
+  def peg_tag(coordinate, color, board_image_size, peg_size, show_position=false)
+    left, top = peg_position(coordinate, board_image_size, peg_size)
+    pos_text = !show_position ? "" : <<-POSITION
+        \n
+        <span style="color:blue;">
+          <%= left.round %><br><%= top.round %>
+        </span>
+        \n
+    POSITION
+    peg_tags = <<-PEGTEXT
+      <div class="#{color}-peg" style="left: #{left}px; top: #{top}px;" >#{pos_text}</div>
+      <div class="peg-shadow-1" style="left: #{left+peg_size/4.0}px; top: #{top-peg_size/8.0}px;" ></div>
+      <div class="peg-shadow-2" style="left: #{left+peg_size/2.0}px; top: #{top-peg_size/4.0}px;" ></div>
+    PEGTEXT
+    peg_tags.html_safe
   end
 end
